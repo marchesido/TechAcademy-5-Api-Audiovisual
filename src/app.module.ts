5e4ff549dbd5574dc438cde172136ca80673d3f7
marchesido <marchesido@gmail.com>
2026-02-21 20:19:01 -0300

    configTypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ProjectEquipmentsModule } from './project-equipments/project-equipments.module';
+import { EquipmentsModule } from './equipments/equipments.module';
+import { ProductionsModule } from './productions/productions.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ClientsModule } from './clients/clients.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      entities: [__dirname + '/database/core/*/.entity{.ts,.js}'],
+      autoLoadEntities: true,
+    }),
+    ClientsModule,
+    ProjectsModule,
+    ProductionsModule,
+    EquipmentsModule,
+    ProjectEquipmentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
