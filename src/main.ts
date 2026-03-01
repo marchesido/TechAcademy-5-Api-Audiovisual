e996dae3c876611fe1470f6efc65bde37c3d7ad4
marchesido <marchesido@gmail.com>
2026-03-01 00:00:22 -0300

    migrationUpdateMain

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({transform:true,disableErrorMessages:false,forbidNonWhitelisted:true}));
   const config = new DocumentBuilder()
     .setTitle('Digital Matricula API')
     .setDescription(
       'Empower your educational experience with the Digital Matricula API. Seamlessly manage courses, academic information, and more with this robust and user-friendly interface. Version 1.0 brings innovative features to enhance your academic journey.',
     )
+
     .setVersion('1.0')
     .addTag('clients', 'Explore and manage courses effortlessly.')
     .addTag('projects', '')
