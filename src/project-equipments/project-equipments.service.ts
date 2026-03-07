64457bf92fe3e39d11fcbd26071d5f9cb415d4c1
marchesido <marchesido@gmail.com>
2026-03-07 17:52:22 -0300

    create-project-dto&service

--- src/project-equipments/project-equipments.service.ts
+++ src/project-equipments/project-equipments.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectEquipmentDto } from './dto/create-project-equipment.dto';
 import { UpdateProjectEquipmentDto } from './dto/update-project-equipment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProjectEquipment } from './entities/project-equipment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProjectEquipmentsService {
-  create(createProjectEquipmentDto: CreateProjectEquipmentDto) {
-    return 'This action adds a new projectEquipment';
+
+  constructor(
+        @InjectRepository(ProjectEquipment)
+        private readonly repository: Repository<ProjectEquipment>,
+      ) {}
+
+  async create(createProjectEquipmentDto: CreateProjectEquipmentDto) {
+    const ProjectEquipment = this.repository.create(createProjectEquipmentDto)
+    return await this.repository.save(ProjectEquipment);
   }
 
-  findAll() {
-    return `This action returns all projectEquipments`;
+  async findAll() {
+    const project_equipment = await this.repository.find();
+    return project_equipment;
   }
 
   findOne(id: number) {
