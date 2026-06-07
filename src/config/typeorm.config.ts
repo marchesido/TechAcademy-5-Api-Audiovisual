5917714f55fc16442b1d914359e587769781d490
marchesido <marchesido@gmail.com>
2026-06-06 21:52:28 -0300

    permanencia de dados

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,9 +8,9 @@ config();
 const configService = new ConfigService();
 
 const AppDataSource = new DataSource({
-  type: 'postgres',
+  type: 'mysql',
   host: configService.get<string>('DB_HOST'),
-  port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 10),
+  port: parseInt(configService.get<string>('DB_PORT') ?? '3306', 10),
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
